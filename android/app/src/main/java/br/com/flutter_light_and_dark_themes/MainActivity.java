c96394ab3b5809d1770686bec119b0192103f555
Filipe Veber <pis0507@supero.com.br>
2020-07-21 23:02:05 -0300

    first commit

---
+++ android/app/src/main/java/br/com/flutter_light_and_dark_themes/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.flutter_light_and_dark_themes;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
